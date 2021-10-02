23f67e9027030287d141b4291405fbca53f3b33b
MichelleRoubikLee <michelleroubiklee@gmail.com>
2021-10-02 18:08:28 -0500

    footer and landing components created and connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/nav/navbar";
+import Landing from "./components/landing/landing"
+import Footer from "./components/footer/footer";
 
 
 function App() {
     return (
         <div className="App">
             <Navbar></Navbar>
+            <Landing></Landing>
+            <Footer></Footer>
         </div>
     )
 }
