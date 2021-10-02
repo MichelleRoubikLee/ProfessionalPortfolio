7938be9a7c554a815c555af6c5af94a491779676
MichelleRoubikLee <michelleroubiklee@gmail.com>
2021-10-02 18:03:41 -0500

    navbar component set up and connected

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/nav/navbar";
+
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar></Navbar>
+        </div>
+    )
+}
+
+export default App;
---
+++ client/src/components/nav/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Navbar(){
+    
+    return(
+        <h1>This is the navbar</h1>
+    )
+}
+
+export default Navbar;
